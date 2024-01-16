87827effc8252a6d3f9d29f1450dcb5fb66e49f9
Thomas Reynolds <treynolds3610@gmail.com>
2024-01-15 21:20:03 -0600

    deploy site

--- gatsby-config.js
+++ gatsby-config.js
@@ -127,13 +127,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-s3`,
       options: {
         bucketName: "thomasreynoldscode.com",
